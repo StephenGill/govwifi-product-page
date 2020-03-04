5cb76538095a275639a8645dcaaa7e2b6712a7fb
Stéphane Maniaci <stephane.maniaci@digital.cabinet-office.gov.uk>
2020-03-04 17:12:18 +0000

    fix: initial stab at the cookies radio REBASE

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,5 +1,35 @@
+function submitCookiePreferences(e) {
+  e.preventDefault();
+
+  const data = new FormData(this);
+
+  for (let category of data.keys()) {
+    GovWifi.cookies.allowCategory(category, data.get(category) === "on");
+  }
+}
+
+function initialCookiePreferences() {
+  if (!GovWifi.cookies.cookiePreferencesDefined()) return;
+
+  const policy = GovWifi.cookies.getCookiePreferences();
+
+  Object.keys(policy).forEach(category => {
+    const value = policy[category] ? "on" : "off";
+    const id = `cookies-${category}-${value}`;
+    const el = document.getElementById(id);
+
+    if (el) el.checked = true;
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   GOVUKFrontend.initAll();
 
   GovWifi.cookies.checkCookiePolicy("cookie-banner");
+
+  document
+    .getElementById("cookie-settings")
+    .addEventListener("submit", submitCookiePreferences);
+
+  initialCookiePreferences();
 });
